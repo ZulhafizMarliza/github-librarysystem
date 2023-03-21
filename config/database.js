572adf7ecb9ec3a5367833eb41973bdf5100753d
Zulhafiz Marliza <zulhafizmarliza@gmail.com>
2023-03-21 13:50:49 +0800

    latest
    push the latest

--- config/database.js
+++ config/database.js
@@ -1,18 +1,61 @@
+// const mysql = require("mysql2");
+
+// // const connection = mysql.createConnection({
+// //   host: "localhost",
+// //   user: "root",
+// //   password: "root",
+// //   database: "library",
+// // });
+
+// const connection = mysql.createConnection({
+//   host: "206.189.148.130",
+//   user: "ejxdbmcj_zmarliza",
+//   password: "TmH#IEz#}P~b",
+//   database: "ejxdbmcj_librarytest",
+//   connectTimeout: 500000, // 10 seconds
+//   acquireTimeout: 500000, // 10 seconds - 10k
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to database: ", err);
+//     return;
+//   }
+//   console.log("Connected to database");
+// });
+
+// module.exports = connection;
+
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "library",
+const pool = mysql.createPool({
+  host: "206.189.148.130",
+  user: "ejxdbmcj_zmarliza",
+  password: "TmH#IEz#}P~b",
+  database: "ejxdbmcj_librarytest",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  idleTimeoutMillis: 500000, // 500 seconds
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error("Error connecting to database: ", err);
     return;
   }
   console.log("Connected to database");
+
+  // Use the connection
+  connection.query("SELECT * FROM tblbook", (err, results, fields) => {
+    if (err) {
+      console.error("Error querying database: ", err);
+      return;
+    }
+    console.log("Results: ", results);
+    // Release the connection back to the pool
+    connection.release();
+  });
 });
 
-module.exports = connection;
+module.exports = pool;
